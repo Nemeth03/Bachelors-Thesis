fc1a1b7baa37e0b490db9a6ef38b885c92bce9e8
nemeth <zdenkon4@gmail.com>
2025-02-24 23:21:18 +0100

    .

--- Code/main.py
+++ Code/main.py
@@ -1,28 +1,49 @@
-
-
-class InputTextFile:
-    def __init__(self, filename):
-        self.filename = filename
-    
-    def read(self):
-        with open(self.filename, 'r', encoding="utf-8") as f:
-            return f.read().strip()
-
-
+# Visualization of the analysed data
 class GraphVisualizer:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        ...
+    
+    def plotLogLog(self):
+        ...
+    
+    def plotLogLin(self):
+        ...
 
+# Tkinter GUI or PyQt6 GUI
 class GUI:
     ...
     
+# Main class that will run the app
 class App:
-    ...
+    def __init__(self):
+        self.inputTextFile = None
+        self.textData = None
+        self.graphVisualizer = None
+        self.gui = None
+        self.languageOptions = ['Slovak', 'English', 'Czech', 'German', 'French', 'Spanish']
+        self.selectedLanguage = ''
+        self.punctuation = {'Period': '.', 
+                            'Comma': ',', 
+                            'Exclamation': '!', 
+                            'Question': '?', 
+                            'Semicolon': ';', 
+                            'Colon': ':', 
+                            'Parenthesis': '()', 
+                            'Apostrophe': "'", 
+                            'Quotation': '"..."',
+                            'Slash': '/', 
+                            'Hyphen': '-',
+                            'Dash': '—'}
+        self.specificPunctuation = {'Slovak': {'Quotation': ['„“']},
+                                    'English': {}, 
+                                    'Czech': {'Quotation': ['„“']},
+                                    'German': {'Quotation': ['„“', '«»']}, 
+                                    'French': {'Quotation': ['«»']}, 
+                                    'Spanish': {'Quotation': ['„“', '«»'], 'Exclamation': ['¡','!'], 'Question': ['¿','?']}}
+    
+    def readTextFile(self, path):
+        with open(path, 'r', encoding="utf-8") as f:
+            return f.read().strip()
     
 if __name__ == '__main__':
-
     print('Running the app...')
-    slovakInput = InputTextFile('pribeh_sk.txt')
-    englishInput = InputTextFile('pribeh_eng.txt')
-    textSk = slovakInput.read()
-    textEng = englishInput.read()
