053ebc4d052cb47c8ef626e3475b82b037a01756
nemeth <zdenkon4@gmail.com>
2025-02-24 15:30:16 +0100

    .

---
+++ Code/main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+class InputTextFile:
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def read(self):
+        with open(self.filename, 'r', encoding="utf-8") as f:
+            return f.read().strip()
+
+
+class GraphVisualizer:
+    def __init__(self, data):
+        self.data = data
+
+class GUI:
+    ...
+    
+class App:
+    ...
+    
+if __name__ == '__main__':
+    print('Running the app...')
+    slovakInput = InputTextFile('pribeh_sk.txt')
+    englishInput = InputTextFile('pribeh_eng.txt')
+    textSk = slovakInput.read()
+    textEng = englishInput.read()
+    
