3da515fa0d9be7020f8ead45d8c65d9540cad687
nemeth <zdenkon4@gmail.com>
2025-02-26 16:41:43 +0100

    .

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
 # TODO
-# najst vhodny vstupny text v anglictine na analyzu, anglictina vhodna kvoli tomu, ze nema sklonovanie
+# najst vhodny vstupny text v anglictine na analyzu (projek guttenberg), anglictina vhodna kvoli tomu, ze nema sklonovanie
 # prestudovat materialy o slovnych sietiach a analyze textu
 # napisat program na generovanie slovnych sieti, moznosti bez alebo vratane interpunkcie
 # pripadna vizualizacia slovnej siete
